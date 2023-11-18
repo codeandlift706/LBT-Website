335b72a958a400be3c7b4d8130c71e77200583b3
Code Queen <channguyen97@gmail.com>
2023-11-17 17:41:11 -0800

    updated main jsx for paths

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css'
 
+// Bringing in the pages the router will use to conditionally show the appropriate views
+import App from './App';
+import Home from './pages/Home';
+import About from './pages/About';
+import Team from './pages/Team';
+import Events from './pages/Events';
+import Contact from './pages/Contact';
+import Error from './pages/Error';
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'team',
+        element: <Team />,
+      },
+      {
+        path: 'events',
+        element: <Events />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
