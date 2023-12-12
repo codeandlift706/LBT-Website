7b1881126bd31eabd03c9e71bbf19725aa055389
Code Queen <channguyen97@gmail.com>
2023-12-11 21:30:19 -0800

    added collapsibles to home page and formatted them

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,27 +2,34 @@ export default function About() {
     return (
         <div className="aboutContainer">
             <header className="aboutHeader">
-                <h2>Our History</h2>
+                <h2 className="historyHeader">Who We Are</h2>
             </header>
-            <div className="startText">
+            <section className="leftText">
+                <h3>What is TNTT</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dignissimos aliquid animi dolore ipsum cumque. Sequi harum dolor corrupti, alias natus provident totam molestias. Optio molestiae sed porro quibusdam vero. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta rem modi tenetur veritatis deleniti, optio laudantium voluptates ad minus dolor dolore sit repellat nesciunt incidunt explicabo excepturi voluptate reiciendis voluptatem? Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident maiores laborum officia, dicta, alias ad quia cum esse ducimus nemo non? Dolore, dignissimos explicabo voluptatum tempore ullam voluptas earum laboriosam!
+                </p>
+            </section>
+            <section className="rightText">
                 <h3>How We Started</h3>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dignissimos aliquid animi dolore ipsum cumque. Sequi harum dolor corrupti, alias natus provident totam molestias. Optio molestiae sed porro quibusdam vero. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta rem modi tenetur veritatis deleniti, optio laudantium voluptates ad minus dolor dolore sit repellat nesciunt incidunt explicabo excepturi voluptate reiciendis voluptatem?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dignissimos aliquid animi dolore ipsum cumque. Sequi harum dolor corrupti, alias natus provident totam molestias. Optio molestiae sed porro quibusdam vero. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta rem modi tenetur veritatis deleniti, optio laudantium voluptates ad minus dolor dolore sit repellat nesciunt incidunt explicabo excepturi voluptate reiciendis voluptatem? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia delectus fugit alias similique minus laboriosam, praesentium quaerat pariatur voluptate magnam dignissimos officia tenetur cupiditate nobis voluptatum error quidem facere amet.
                 </p>
-            </div>
-            <div className="missionText">
+            </section>
+
+            <section className="leftText">
                 <h3>Our Mission</h3>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dignissimos aliquid animi dolore ipsum cumque. Sequi harum dolor corrupti, alias natus provident totam molestias. Optio molestiae sed porro quibusdam vero. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta rem modi tenetur veritatis deleniti, optio laudantium voluptates ad minus dolor dolore sit repellat nesciunt incidunt explicabo excepturi voluptate reiciendis voluptatem?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dignissimos aliquid animi dolore ipsum cumque. Sequi harum dolor corrupti, alias natus provident totam molestias. Optio molestiae sed porro quibusdam vero. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta rem modi tenetur veritatis deleniti, optio laudantium voluptates ad minus dolor dolore sit repellat nesciunt incidunt explicabo excepturi voluptate reiciendis voluptatem? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat saepe officia, cumque doloremque ea, alias velit voluptatem error quia ipsum omnis, vitae tempora enim minima totam laborum deserunt quis. Architecto.
                 </p>
-            </div>
+            </section>
 
-            <div className="doText">
+            <section className="rightText">
                 <h3>What We Do</h3>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dignissimos aliquid animi dolore ipsum cumque. Sequi harum dolor corrupti, alias natus provident totam molestias. Optio molestiae sed porro quibusdam vero. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta rem modi tenetur veritatis deleniti, optio laudantium voluptates ad minus dolor dolore sit repellat nesciunt incidunt explicabo excepturi voluptate reiciendis voluptatem?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dignissimos aliquid animi dolore ipsum cumque. Sequi harum dolor corrupti, alias natus provident totam molestias. Optio molestiae sed porro quibusdam vero. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta rem modi tenetur veritatis deleniti, optio laudantium voluptates ad minus dolor dolore sit repellat nesciunt incidunt explicabo excepturi voluptate reiciendis voluptatem? Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi velit odio vitae rerum dolores facilis, soluta at atque voluptatum, corrupti quo recusandae quis debitis exercitationem, doloremque eos voluptates culpa accusamus.
                 </p>
-            </div>
+            </section>
         </div>
     );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,48 @@
 // Bringing in the required component from 'react-router-dom' for linking between pages
 import { Link } from 'react-router-dom';
+import React from 'react';
+import Collapsible from 'react-collapsible';
 
 export default function Home() {
     return (
         <>
             <section id="home">
-                <section className="homeContainer">
-                    {/* <h2 className="homeHeading">Who We Are:</h2> */}
-                    {/* <h2 className="homeHeading">Est. 1989 | Thiếu Nhi Thánh Thể | Gương Mẫu</h2> */}
-                </section>
             </section>
+            <div className="homeContainer">
+
+
+<section>
+                <Collapsible trigger="What is Thiếu Nhi Thánh Thể?" className="collapsible">
+                <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam nulla praesentium? Natus possimus, voluptate aliquam quaerat architecto illum esse non placeat cumque. Totam iste, debitis molestiae architecto vel veniam. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis nemo ex, nihil adipisci eveniet aperiam ut tenetur quo. Modi magni et temporibus eligendi odit excepturi fuga nisi quisquam amet tempore. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis, distinctio, hic facere expedita quos quisquam laboriosam eveniet autem asperiores harum ipsam eius est maiores, ducimus quod mollitia obcaecati nihil sapiente?
+                    </p>
+                </Collapsible>
+</section>
+
+<section>
+                <Collapsible trigger="What is Gương Mẫu?" className="collapsible">
+                <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam nulla praesentium? Natus possimus, voluptate aliquam quaerat architecto illum esse non placeat cumque. Totam iste, debitis molestiae architecto vel veniam. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis nemo ex, nihil adipisci eveniet aperiam ut tenetur quo. Modi magni et temporibus eligendi odit excepturi fuga nisi quisquam amet tempore. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis, distinctio, hic facere expedita quos quisquam laboriosam eveniet autem asperiores harum ipsam eius est maiores, ducimus quod mollitia obcaecati nihil sapiente?
+                    </p>
+                </Collapsible>
+</section>
+
+{/* 
+
+
+                <section>
+                    <h2>What is Thiếu Nhi Thánh Thể?</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam nulla praesentium? Natus possimus, voluptate aliquam quaerat architecto illum esse non placeat cumque. Totam iste, debitis molestiae architecto vel veniam. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis nemo ex, nihil adipisci eveniet aperiam ut tenetur quo. Modi magni et temporibus eligendi odit excepturi fuga nisi quisquam amet tempore. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis, distinctio, hic facere expedita quos quisquam laboriosam eveniet autem asperiores harum ipsam eius est maiores, ducimus quod mollitia obcaecati nihil sapiente?
+                    </p>
+                </section>
+                <section>
+                    <h2>What is Gương Mẫu?</h2>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus hic accusantium dolor repudiandae sequi iusto distinctio, illo nostrum quia incidunt eum dolorem commodi veniam, sed in harum, porro laboriosam aspernatur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, tenetur molestiae, repellendus culpa voluptatem eveniet voluptas labore, quisquam cumque suscipit obcaecati eum fugit doloribus voluptate ipsa ducimus amet dolorem at. Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis at nulla iusto ad, debitis sequi nesciunt accusamus architecto natus nisi omnis rem cupiditate. Doloribus quo, aut accusantium id autem quas?
+                    </p>
+                </section> */}
+            </div>
         </>
     );
 }
