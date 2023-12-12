eae4388574f236d1601f8353a84e105d14b63c26
Code Queen <channguyen97@gmail.com>
2023-12-11 20:19:52 -0800

    added containers to about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,10 +1,25 @@
 export default function About() {
     return (
-        <div>
-            <h1>About Page</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dignissimos aliquid animi dolore ipsum cumque. Sequi harum dolor corrupti, alias natus provident totam molestias. Optio molestiae sed porro quibusdam vero. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta rem modi tenetur veritatis deleniti, optio laudantium voluptates ad minus dolor dolore sit repellat nesciunt incidunt explicabo excepturi voluptate reiciendis voluptatem?
-            </p>
+        <div className="aboutContainer">
+            <div className="startText">
+                <h3>How We Started</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dignissimos aliquid animi dolore ipsum cumque. Sequi harum dolor corrupti, alias natus provident totam molestias. Optio molestiae sed porro quibusdam vero. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta rem modi tenetur veritatis deleniti, optio laudantium voluptates ad minus dolor dolore sit repellat nesciunt incidunt explicabo excepturi voluptate reiciendis voluptatem?
+                </p>
+            </div>
+            <div className="missionText">
+                <h3>Our Mission</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dignissimos aliquid animi dolore ipsum cumque. Sequi harum dolor corrupti, alias natus provident totam molestias. Optio molestiae sed porro quibusdam vero. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta rem modi tenetur veritatis deleniti, optio laudantium voluptates ad minus dolor dolore sit repellat nesciunt incidunt explicabo excepturi voluptate reiciendis voluptatem?
+                </p>
+            </div>
+
+            <div className="doText">
+                <h3>What We Do</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dignissimos aliquid animi dolore ipsum cumque. Sequi harum dolor corrupti, alias natus provident totam molestias. Optio molestiae sed porro quibusdam vero. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta rem modi tenetur veritatis deleniti, optio laudantium voluptates ad minus dolor dolore sit repellat nesciunt incidunt explicabo excepturi voluptate reiciendis voluptatem?
+                </p>
+            </div>
         </div>
     );
 }
